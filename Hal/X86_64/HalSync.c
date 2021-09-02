1b48b4abc3ebcd474863d7f6d8f36fe2f5150aee
LMOS <609379965@qq.com>
2021-09-02 17:45:25 +0800

    增加Hal层的自旋锁、中断等同步操作

---
+++ Hal/X86_64/HalSync.c
@@ -0,0 +1,124 @@
+/**********************************************************
+		同步文件HalSync.c
+***********************************************************
+				彭东
+**********************************************************/
+#include "BaseType.h"
+#include "HalCPU.h"
+#include "HalSync.h"
+
+void HalHalt()
+{
+    HALT();
+}
+
+void HalCliHalt()
+{
+    CLIHALT();
+}
+
+void HalStiHalt()
+{
+    STIHALT();
+}
+
+void HalSti()
+{
+    STI();
+}
+
+void HalCli()
+{
+    CLI();
+}
+
+
+void HalStiFlags(CPUFlg* cpuflg)
+{
+    HalRestoreFlagsSti(cpuflg);
+}
+
+void HalCliFlags(CPUFlg* cpuflg)
+{
+    HalSaveFlagsCli(cpuflg);
+}
+
+void HalFlagsSti(CPUFlg* cpuflg)
+{
+    HalSaveFlagsSti(cpuflg);
+}
+
+void HalFlagsCli(CPUFlg* cpuflg)
+{
+    HalRestoreFlagsCli(cpuflg);
+}
+
+void HalSPinLockInit(SPinLock *init)
+{
+    init->Lock = 0;
+    return;
+}
+
+void HalSPinLock(SPinLock *lock)
+{
+    __asm__ __volatile__(
+        "1:         \n"
+        "lock; xchg  %0, %1 \n"
+        "cmpl   $0, %0      \n"
+        "jnz    2f      \n"
+        ".section .spinlock.text,"
+        "\"ax\""
+        "\n"                    // 重新定义一个代码段所以jnz 2f下面并不是
+        "2:         \n"         //cmpl $0,%1 事实上下面的代码不会常常执行,
+        "cmpl   $0, %1      \n" //这是为了不在cpu指令高速缓存中填充无用代码
+        "jne    2b      \n"     //要知道那可是用每位6颗晶体管做的双极性静态
+        "jmp    1b      \n"     //储存器,比内存条快几千个数量级
+        ".previous      \n"
+        :
+        : "r"(1), "m"(*lock));
+    return;
+}
+
+void HalUnSPinLock(SPinLock *lock)
+{
+    __asm__ __volatile__(
+        "movl   $0, %0\n"
+        :
+        : "m"(*lock));
+    return;
+}
+
+void HalSPinLockSaveFlagsCli(SPinLock *lock, CPUFlg *cpuflg)
+{
+    __asm__ __volatile__(
+        "pushfq             \n\t"
+        "cli                \n\t"
+        "popq %0            \n\t"
+
+        "1:                 \n\t"
+        "lock; xchg  %1, %2 \n\t"
+        "cmpl   $0,%1       \n\t"
+        "jnz    2f          \n\t"
+        ".section .spinlock.text,"
+        "\"ax\""
+        "\n\t"                    //重新定义一个代码段所以jnz 2f下面并不是
+        "2:                 \n\t" //cmpl $0,%1 事实上下面的代码不会常常执行,
+        "cmpl   $0,%2       \n\t" //这是为了不在cpu指令高速缓存中填充无用代码
+        "jne    2b          \n\t"
+        "jmp    1b          \n\t"
+        ".previous          \n\t"
+        : "=m"(*cpuflg)
+        : "r"(1), "m"(*lock));
+    return;
+}
+
+void HalUnSPinLockRestoreFlagsSti(SPinLock *lock, CPUFlg *cpuflg)
+{
+    __asm__ __volatile__(
+        "movl   $0, %0\n\t"
+        "pushq %1 \n\t"
+        "popfq \n\t"
+        :
+        : "m"(*lock), "m"(*cpuflg));
+    return;
+}
