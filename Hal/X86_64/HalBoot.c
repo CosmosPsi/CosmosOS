7b875345c10bdde45d3e995d628a3d0ffe289df7
LMOS <609379965@qq.com>
2021-09-08 17:30:13 +0800

    增加启动引导HalBootStart()相关代码

---
+++ Hal/X86_64/HalBoot.c
@@ -0,0 +1,28 @@
+/**********************************************************
+		底层引导初始化文件HalBoot.c
+***********************************************************
+				彭东
+**********************************************************/
+#include "BaseType.h"
+#include "HalBoot.h"
+#include "HalCPU.h"
+#include "HalMMU.h"
+#include "HalCache.h"
+#include "HalIPC.h"
+#include "KrlInit.h"
+
+private Bool HalBootInit()
+{
+	HalCPUInit();
+	HalMMUInit();
+	HalCacheInit();
+	HalIPCInit();
+	return TRUE;
+}
+
+public void HalBootStart()
+{
+	HalBootInit();
+	KrlCoreInit();
+	return;
+}
