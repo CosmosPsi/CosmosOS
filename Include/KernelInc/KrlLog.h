c19ed4db835def14a36a3b275ca99e1e273a6675
LMOS <609379965@qq.com>
2021-09-13 16:25:44 +0800

    增加内核日志相关代码

---
+++ Include/KernelInc/KrlLog.h
@@ -0,0 +1,22 @@
+/****************************************************************
+        Kernel层日志头文件KrlLog.h
+*****************************************************************
+                彭东 
+****************************************************************/
+
+#ifndef _KRLLOGHEAD
+#define _KRLLOGHEAD
+
+typedef __builtin_va_list VAList;
+#define VA_START(ap,np) __builtin_va_start(ap,np)
+#define VA_END(ap) __builtin_va_end(ap)
+#define VA_ARG(ap,ty) __builtin_va_arg(ap,ty)
+
+private Char* FmtNumber(Char* str, UInt n, SInt base);
+private Char* FmtStrCopy(Char* buf, Char* str);
+private void FmtCore(Char* buf, const Char* fmt, VAList args);
+public void KrlPrint(const Char* fmt, ...);
+public void KrlLog(UInt level, const Char* fmt, ...);
+public Bool KrlLogInit();
+
+#endif
---
+++ Kernel/KrlLog.c
@@ -0,0 +1,114 @@
+/****************************************************************
+        Kernel层日志文件KrlLog.c
+*****************************************************************
+                彭东 
+****************************************************************/
+#include "BaseType.h"
+#include "HalSync.h"
+#include "HalFirmware.h"
+#include "KrlLog.h"
+
+private Char* FmtNumber(Char* str, UInt n, SInt base)
+{
+	register Char* p;
+	Char strbuf[36];
+	p = &strbuf[36];
+	*--p = 0;
+	if (n == 0)
+	{
+		*--p = '0';
+	}
+	else
+	{
+		do
+		{
+			*--p = "0123456789abcdef"[n % base];
+		} while (n /= base);
+	}
+	while (*p != 0)
+	{
+		*str++ = *p++;
+	}
+	return str;
+}
+
+private Char* FmtStrCopy(Char* buf, Char* str)
+{
+	while(*str)
+	{
+		*buf = *str;
+		buf++;
+		str++;
+	}
+	return buf;
+}
+
+private void FmtCore(Char* buf, const Char* fmt, VAList args)
+{
+	Char* p = buf;
+	while(*fmt)
+	{
+		if(*fmt != '%')
+		{
+			*p++ = *fmt++;
+			continue;
+		}
+		fmt++;
+		switch(*fmt)
+		{
+		case 'x':
+			p = FmtNumber(p, VA_ARG(args, UInt), 16);
+			fmt++;
+			break;
+		case 'd':
+			p = FmtNumber(p, VA_ARG(args, UInt), 10);
+			fmt++;
+			break;
+		case 's':
+			p = FmtStrCopy(p, (Char*)VA_ARG(args, UInt));
+			fmt++;
+			break;
+		default:
+			break;
+		}
+	}
+	*p = 0;
+	return;
+}
+
+public void KrlPrint(const Char* fmt, ...)
+{
+	CPUFlg cpuflg;
+	Char buf[512];
+	VAList ap;
+	HalCliFlags(&cpuflg);
+	VA_START(ap, fmt);
+	FmtCore(buf, fmt, ap);
+	StringWrite(&kdftgh, buf);
+	VA_END(ap);
+	HalStiFlags(&cpuflg);
+	return;
+}
+
+public void KrlLog(UInt level, const Char* fmt, ...)
+{
+    CPUFlg cpuflg;
+	Char buf[512];
+	VAList ap;
+    if(0 != level)
+    {
+        return;
+    }
+	HalCliFlags(&cpuflg);
+	VA_START(ap, fmt);
+	FmtCore(buf, fmt, ap);
+	StringWrite(&kdftgh, buf);
+	VA_END(ap);
+	HalStiFlags(&cpuflg);
+    return;
+}
+
+public Bool KrlLogInit()
+{
+    return TRUE;
+}
