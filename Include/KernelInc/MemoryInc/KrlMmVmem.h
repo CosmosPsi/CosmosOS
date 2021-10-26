0a9b9ced30b19a07221ea655ef8dae1ff41a2fed
LMOS <609379965@qq.com>
2021-10-26 11:07:15 +0800

    增加VPB、VBM数据结构

--- Include/KernelInc/MemoryInc/KrlMmVmem.h
+++ Include/KernelInc/MemoryInc/KrlMmVmem.h
@@ -9,6 +9,40 @@
 #include "RBTree.h"
 #include "Atomic.h"
 
+
+// Virtual Memory PMSAD(Page) Box Manager
+typedef struct VBM 
+{
+	List Lists;
+	MLock Lock;
+	UInt Status;
+	UInt Flags;
+	UInt VPBNR;
+	List VPBHead;
+	UInt VPBCacheNR;
+	UInt VPBCacheMax;
+	UInt VPBCacheMin;
+	List VPBCacheHead;
+	void* Ext;
+}VBM;
+
+// Virtual Memory PMSAD(Page) Box
+typedef struct VPB 
+{
+	List Lists;
+	MLock Lock;
+	UInt Status;
+	UInt Flags;
+    RefCount Count;
+    UInt Type;
+	UInt PmsadNR;
+	List PmsadLists;
+	VBM* ParentVBM;
+	void* FileNode;
+	void* ObjExecer;
+	void* Ext;
+}VPB;
+
 // Virtual Address Descriptors
 typedef struct VAD
 {
@@ -20,7 +54,7 @@ typedef struct VAD
     U64   Access;
     Addr  Start;
     Addr  End;
-    void* PMSADBox;
+    VPB*  PMSADBox;
     VAM*  ParentVAM;
     void* InnerOBJ; 
 }VAD;
