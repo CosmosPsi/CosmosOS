8db9ed14b8fc2d4dbfedf87597e19e8b72ad948d
LMOS <609379965@qq.com>
2021-11-03 16:28:44 +0800

    增加ELock数据结构

--- Include/KernelInc/ExecutorInc/KrlExecutorManage.h
+++ Include/KernelInc/ExecutorInc/KrlExecutorManage.h
@@ -3,3 +3,18 @@
 ***********************************************************
                 彭东
 **********************************************************/
+#ifndef _KRLEXECUTORMANAGEHEAD
+#define _KRLEXECUTORMANAGEHEAD
+#include "HalSync.h"
+
+#define EXECUTORNODE_MAX (64)
+#define EXECUTORDATA_SECTION __attribute__((section(".executor.data")))
+#define DefinedEXECUTORData(vartype,varname) EXECUTORDATA_SECTION vartype varname
+
+typedef struct ELOCK
+{
+    SPinLock Locks;
+    // SEM
+}ELock;
+
+#endif
