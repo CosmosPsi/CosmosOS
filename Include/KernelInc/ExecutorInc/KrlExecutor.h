a2642bab583e9fd692560671b93054e58ac876a6
LMOS <609379965@qq.com>
2021-11-05 16:24:17 +0800

    增加ThreadBox数据结构

--- Include/KernelInc/ExecutorInc/KrlExecutor.h
+++ Include/KernelInc/ExecutorInc/KrlExecutor.h
@@ -25,5 +25,20 @@ typedef struct THREADHEAD
      List ThreadLists;
 }ThreadHead;
 
+typedef struct THREADBOX
+{
+    ELock Lock;
+	U64 Status;
+	U64 Flags;
+    UInt ThreadSumNR;
+    ThreadHead NewHead;
+    ThreadHead RunHead;
+    ThreadHead SleepHead;
+    ThreadHead BlockHead;
+    ThreadHead WaitHead;
+    ThreadHead DeadHead;
+    ThreadHead CacheHead;
+}ThreadBox;
+
 
 #endif
