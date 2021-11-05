6c5a5785f61357e912843a81f2dd37a91847309d
LMOS <609379965@qq.com>
2021-11-05 16:22:13 +0800

    增加ExAffiliation数据结构

---
+++ Include/KernelInc/ExecutorInc/KrlExecutor.h
@@ -0,0 +1,24 @@
+/**********************************************************
+        执行体头文件KrlExecutor.h
+***********************************************************
+                彭东
+**********************************************************/
+#ifndef _KRLEXECUTORHEAD
+#define _KRLEXECUTORHEAD
+
+#include "KrlExecutorManage.h"
+
+typedef struct EXAFFILIATION
+{
+    UInt CPUID;
+    UInt MNodeID;
+    ExecutorBox* ExecutorBoxPtr;
+    ExecutorNode* ExecutorNodePtr;
+    //User
+    //Transfer
+    //Group
+}ExAffiliation;
+
+
+
+#endif
