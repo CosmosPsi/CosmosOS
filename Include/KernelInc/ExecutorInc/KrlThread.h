41e350a30916fb42dae62fa746021854185f2ec1
LMOS <609379965@qq.com>
2021-11-11 17:19:37 +0800

    增加TAffiliation结构

---
+++ Include/KernelInc/ExecutorInc/KrlThread.h
@@ -0,0 +1,20 @@
+/**********************************************************
+        线程头文件KrlThread.h
+***********************************************************
+                彭东
+**********************************************************/
+#ifndef _KRLTHREADHEAD
+#define _KRLTHREADHEAD
+
+typedef struct TAFFILIATION
+{
+    UInt CPUID;
+    UInt MNodeID;
+    Executor* ExecutorPtr;
+    //User
+    //Transfer
+    //Group
+}TAffiliation;
+
+
+#endif
