79a41b4212b802b6e4be927d8d020378e53fca5e
LMOS <609379965@qq.com>
2021-12-02 14:06:03 +0800

    增加KrlSeManage模块

---
+++ Kernel/Services/KrlSeManage.c
@@ -0,0 +1,15 @@
+/**********************************************************
+        系统服务管理文件KrlSeManage.c
+***********************************************************
+                彭东
+**********************************************************/
+#include "BaseType.h"
+#include "KrlExecutor.h"
+#include "KrlThread.h"
+#include "KrlSeManage.h"
+
+public Bool KrlSeManageInit()
+{
+
+    return TRUE;
+} 
