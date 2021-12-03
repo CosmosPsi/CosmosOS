7e023bc89814129c259bb1e0dc0e8b7ebb56d0e8
LMOS <609379965@qq.com>
2021-12-03 16:10:52 +0800

    修正KrlSeCreateServiceOnServiceInfo函数-1

--- Kernel/Services/KrlSeManage.c
+++ Kernel/Services/KrlSeManage.c
@@ -4,6 +4,9 @@
                 彭东
 **********************************************************/
 #include "BaseType.h"
+#include "List.h"
+#include "HalCPU.h"
+#include "KrlMmManage.h"
 #include "KrlExecutorManage.h"
 #include "KrlExecutor.h"
 #include "KrlThread.h"
@@ -15,35 +18,21 @@ public Executor* KrlSeCreateServiceOnServiceInfo(ServiceInfo* info)
     Executor* executor = NULL;
     Thread* thread = NULL;
     IF_NULL_RETURN_NULL(info);
-
-    executor = KrlExCreateExecutor();
-    IF_NULL_RETURN_NULL(executor);
-    thread = KrlExCreateThread();
-    if(NULL == thread)
+    if(NULL == info->Env.AffiExecutor)
+    {
+        executor = KrlExCreateExecutor();
+        IF_NULL_RETURN_NULL(executor);
+        info->Env.AffiExecutor = executor;
+    }
+    
+    if(KrlExCreateThreadInitRunEnv(&info->Env) == FALSE)
     {
         IF_NEQ_DEAD(TRUE, KrlExDestroyExecutor(executor), "KrlExDestroyExecutor Is Fail\n");
         return NULL;
     }
 
-    if(KrlExThreadInitRunEnv(thread, &info->Env) == FALSE)
-    {
-        IF_NEQ_DEAD(TRUE, KrlExDestroyThread(thread), "KrlExDestroyThread Is Fail\n");
-        IF_NEQ_DEAD(TRUE, KrlExDestroyExecutor(executor), "KrlExDestroyExecutor Is Fail\n");
-        return NULL;
-    }
-
-    KrlExThreadSetRunStatus(thread, THREAD_NEW_STATUS);
-
-    if(KrlExThreadAddToExecutor(executor, thread) == FALSE)
-    {
-        IF_NEQ_DEAD(TRUE, KrlExDestroyThread(thread), "KrlExDestroyThread Is Fail\n");
-        IF_NEQ_DEAD(TRUE, KrlExDestroyExecutor(executor), "KrlExDestroyExecutor Is Fail\n");
-        return NULL;
-    }
-
-
-    info->Id = (UInt)executor;
-    return executor;
+    info->Id = (UInt)info->Env.AffiExecutor;
+    return (Executor*)(info->Env.AffiExecutor);
 }
 
 public Executor* KrlSeCreateSystemIdleService(ServiceInfo* info)
@@ -81,14 +70,16 @@ public Executor* KrlSeCreateSystemIdleService(ServiceInfo* info)
 
     KrlExAddCPUIdleExecutor(executor);
     KrlExSetCurrentCPUIdleTransfer(&thread->ThreadTransfer);
-    
+
     info->Id = (UInt)executor;
     return executor;
 }
 
 public Bool KrlSeStartAllRunService()
 {
-    ServiceInfo* serinfo = NULL;
+    ServiceInfo* serinfostart = NULL;
+    ServiceInfo* serinfoend = NULL;
+
 }
 
 public Bool KrlSeManageInit()
