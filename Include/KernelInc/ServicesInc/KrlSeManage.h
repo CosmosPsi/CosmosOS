b5d31c913ec3c0a88b35c22ef40db230214974f1
LMOS <609379965@qq.com>
2021-12-03 17:38:22 +0800

    增加DEFINE_SYSIDLE_SERVICE、DEFINE_KERNEL_SERVICE、DEFINE_USER_SERVICE宏

--- Include/KernelInc/ServicesInc/KrlSeManage.h
+++ Include/KernelInc/ServicesInc/KrlSeManage.h
@@ -17,9 +17,16 @@ typedef struct SERVICEINFO
 
 #define SERVICE_IDLE_FLAG (1)
 #define SETRUNENV(flags, executor, start, cpumode) 0,flags,executor,start,cpumode,0,0
-#define DEFINE_SERVICE(typename, flags, env, sename) \
-__attribute__((section(".service.data"))) ServiceInfo typename = {0, flags, 0, {env}, sename}
 
+#define KERNELTRUNENV(start) SETRUNENV(0, NULL, start, KERNEL_CPU_MODE)
+#define USERTRUNENV(start) SETRUNENV(0, NULL, start, USER_CPU_MODE)
+
+#define DEFINE_SERVICE(typename, flags, env, sename) \
+__attribute__((section(".service.data"))) ServiceInfo typename = {0, flags, 0, {env}, "##sename##"}
+
+#define DEFINE_SYSIDLE_SERVICE(start, sename) DEFINE_SERVICE(ServiceObj##sename, SERVICE_IDLE_FLAG, KERNELTRUNENV(start), sename)
+#define DEFINE_KERNEL_SERVICE(start, sename) DEFINE_SERVICE(ServiceObj##sename, 0, KERNELTRUNENV(start), sename)
+#define DEFINE_USER_SERVICE(start, sename) DEFINE_SERVICE(ServiceObj##sename, 0, USERTRUNENV(start), sename)
 public Executor* KrlSeCreateServiceOnServiceInfo(ServiceInfo* info);
 public Executor* KrlSeCreateSystemIdleService(ServiceInfo* info);
 public Bool KrlSeStartAllRunService();
