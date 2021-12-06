20e7ce8af9696365e6c53be6bc4d5007da028951
LMOS <609379965@qq.com>
2021-12-06 11:36:18 +0800

    完善KrlCoreInit函数

--- Kernel/KrlInit.c
+++ Kernel/KrlInit.c
@@ -4,9 +4,21 @@
                 彭东 
 ****************************************************************/
 #include "BaseType.h"
+#include "KrlMmManage.h"
+#include "KrlMmPool.h"
+#include "KrlMmVmem.h"
+#include "KrlExecutorManage.h"
+#include "KrlTransfer.h"
+#include "KrlSeManage.h"
 #include "KrlInit.h"
 
 public void KrlCoreInit()
 {
+    IF_NEQ_DEAD(TRUE, KrlMmManageInit(), "KrlMmManageInit Is Fail\n");
+    IF_NEQ_DEAD(TRUE, KrlMmPoolInit(), "KrlMmPoolInit Is Fail\n");
+    IF_NEQ_DEAD(TRUE, KrlMmVMemInit(), "KrlMmVMemInit Is Fail\n");
+    IF_NEQ_DEAD(TRUE, KrlExecutorManageInit(), "KrlExecutorManageInit Is Fail\n");
+    IF_NEQ_DEAD(TRUE, KrlTransferInit(), "KrlTransferInit Is Fail\n");
+    IF_NEQ_DEAD(TRUE, KrlSeManageInit(), "KrlSeManageInit Is Fail\n");
     return;
 }
