af5da7c201f79121a58118dbe36458cb7f05c794
LMOS <609379965@qq.com>
2021-12-07 14:57:07 +0800

    增加EWaitList结构

---
+++ Include/KernelInc/ExecutorInc/KrlExecutorSync.h
@@ -0,0 +1,15 @@
+/**********************************************************
+        执行体同步头文件KrlExecutorSync.h
+***********************************************************
+                彭东
+**********************************************************/
+#ifndef _KRLEXECUTORSYNCHEAD
+#define _KRLEXECUTORSYNCHEAD
+
+typedef struct EWAITLIST
+{
+    UInt WaitNR;
+    List Lists;
+}EWaitList;
+
+#endif
